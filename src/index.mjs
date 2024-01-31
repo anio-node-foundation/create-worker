e7d38482cc18f758f07df4c0f2f4799c83a160b9
dasdeo <mark@anio.software>
2024-01-31 21:59:11 +0100

    Attempt to break dependence of external file "bootstrap.mjs".

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,33 @@
 import createPromise from "@anio-js-core-foundation/create-promise"
-import createNodeWorkerProcess from "./createNodeWorkerProcess.mjs"
+import {fileURLToPath} from "node:url"
+import {spawn} from "node:child_process"
+import bootstrap from "./bootstrap.mjs"
+
+const __filename = fileURLToPath(import.meta.url)
+
+if ("send" in process) {
+	bootstrap()
+}
+
+function createNodeWorkerProcess(options) {
+	let node_binary_path = "node", silent = true
+
+	if ("node_binary" in options) {
+		node_binary_path = options.node_binary
+	}
+
+	if ("silent" in options) {
+		silent = options.silent
+	}
+
+	const stdio = silent ? ["pipe", "pipe", "pipe", "ipc"] : ["pipe", "inherit", "inherit", "ipc"]
+
+	const child = spawn(node_binary_path, [__filename], {
+		stdio
+	})
+
+	return child
+}
 
 function createWorkerInstance({
 	child,
